bacabe9236ff08178c31c42f64e1ef9b03c0d855
Andrés González <agonzalez@spredfast.com>
2018-08-18 16:42:08 -0300

    Add persist functionallity and refactor to Amount and Rate to Float

--- app/src/main/java/io/palan/juntala/ExchangeRatesStore.kt
+++ app/src/main/java/io/palan/juntala/ExchangeRatesStore.kt
@@ -1,36 +1,68 @@
 package io.palan.juntala
 
+import android.content.SharedPreferences
+
 class ExchangeRatesStore(val exchangeRates: ExchangeRatesMap) {
 
     companion object {
-        fun ofDefault(): ExchangeRatesStore {
-            return ExchangeRatesStore(DEFAULT_EXCHANGE_RATES)
+        val default: ExchangeRatesStore = ExchangeRatesStore(DEFAULT_EXCHANGE_RATES)
+
+        fun of(preferences: SharedPreferences): ExchangeRatesStore {
+            val eurArsRate = preferences
+                    .getFloat("EUR:ARS", DEFAULT_EXCHANGE_RATES.getValue("EUR" to "ARS"))
+            val eurUsdRate = preferences
+                    .getFloat("EUR:USD", DEFAULT_EXCHANGE_RATES.getValue("EUR" to "USD"))
+
+            val exchangeRatesMap: ExchangeRatesMap = mutableMapOf(
+                    ("EUR" to "ARS") to eurArsRate,
+                    ("EUR" to "USD") to eurUsdRate
+            ).withDefault { _ -> 1.0f }
+
+            return ExchangeRatesStore(exchangeRatesMap)
         }
     }
 
     fun set(exchangeRate: ExchangeRate) {
         exchangeRates[exchangeRate.first] = exchangeRate.second
     }
+
+    fun set(exchangeKey: ExchangeKey, rate: String) {
+        exchangeRates[exchangeKey] = rate.toFloatOrNull() ?: exchangeRates.getValue(exchangeKey)
+    }
+
+    fun persist(preferences: SharedPreferences) {
+        with(preferences.edit()) {
+            putFloat("EUR:ARS", exchangeRates.getValue("EUR" to "ARS"))
+            putFloat("EUR:USD", exchangeRates.getValue("EUR" to "USD"))
+            apply()
+        }
+    }
 }
 
 typealias Currency = String
-typealias Amount = Double
-typealias Rate = Double
+typealias Amount = Float
+typealias Rate = Float
 typealias ExchangeKey = Pair<Currency, Currency>
 typealias ExchangeRate = Pair<ExchangeKey, Rate>
 typealias ExchangeRatesMap = MutableMap<ExchangeKey, Rate>
 
 class Price(val currency: Currency, val amount: Amount) {
+    companion object {
+        fun of(currency: Currency, amount: String): Price {
+            return Price(currency, amount.toFloatOrNull() ?: 0.0f)
+        }
+    }
+
     fun convert(to: Currency, exchangeRates: ExchangeRatesMap): Price {
         return Price(to, amount * exchangeRates.getValue(currency to to))
     }
 }
 
 val DEFAULT_EXCHANGE_RATES = mutableMapOf(
-        ("ARS" to "EUR") to 0.029,
-        ("ARS" to "USD") to 0.033,
-        ("EUR" to "ARS") to 33.97,
-        ("EUR" to "USD") to 1.14,
-        ("USD" to "ARS") to 29.86,
-        ("USD" to "EUR") to 0.8
-).withDefault { _ -> 1.0 }
+        ("ARS" to "EUR") to 0.029f,
+        ("ARS" to "USD") to 0.033f,
+        ("EUR" to "ARS") to 33.97f,
+        ("EUR" to "USD") to 1.14f,
+        ("USD" to "ARS") to 29.86f,
+        ("USD" to "EUR") to 0.8f
+).withDefault { _ -> 1.0f }
--- app/src/main/java/io/palan/juntala/MainActivity.kt
+++ app/src/main/java/io/palan/juntala/MainActivity.kt
@@ -1,43 +1,46 @@
 package io.palan.juntala
 
+import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    private val exchangeRatesStore: ExchangeRatesStore = ExchangeRatesStore.ofDefault()
+    private var exchangeRatesStore: ExchangeRatesStore = ExchangeRatesStore.default
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        exchangeRatesStore = ExchangeRatesStore.of(getPreferences(Context.MODE_PRIVATE))
 
         eur_ars_input.hint = exchangeRatesStore.exchangeRates.getValue("EUR" to "ARS").toString()
         eur_usd_input.hint = exchangeRatesStore.exchangeRates.getValue("EUR" to "USD").toString()
     }
 
     fun handleCalculate(view: View) {
-        val inputValue = amount_from_input.text.toString().toDoubleOrNull() ?: 0.0
-        val price = Price("EUR", inputValue)
+        val price = Price.of("EUR", amount_from_input.text.toString())
         val exchangeRates = exchangeRatesStore.exchangeRates
         amount_converted_pesos.text = price.convert("ARS", exchangeRates).amount.toString()
         amount_converted_dollars.text = price.convert("USD", exchangeRates).amount.toString()
     }
 
     fun handleChangeRates(view: View) {
-        val previousEurArsChangeRate = exchangeRatesStore.exchangeRates.getValue("EUR" to "ARS")
-        val eurArsChangeRate = eur_ars_input.text.toString().toDoubleOrNull()
-                ?: previousEurArsChangeRate
+        exchangeRatesStore.set(("EUR" to "ARS"), eur_ars_input.text.toString())
+        exchangeRatesStore.set(("EUR" to "USD"), eur_usd_input.text.toString())
 
-        val previousEurUsdChangeRate = exchangeRatesStore.exchangeRates.getValue("EUR" to "USD")
-        val eurUsdChangeRate = eur_usd_input.text.toString().toDoubleOrNull()
-                ?: previousEurUsdChangeRate
-
-        exchangeRatesStore.set(("EUR" to "ARS") to eurArsChangeRate)
-        exchangeRatesStore.set(("EUR" to "USD") to eurUsdChangeRate)
+        with(eur_ars_input) { hint = if (text.isEmpty()) hint else text }
+        with(eur_usd_input) { hint = if (text.isEmpty()) hint else text }
 
         handleCalculate(view)
+        exchangeRatesStore.persist(getPreferences(Context.MODE_PRIVATE))
+    }
+
+    fun handleFetch(view: View) {
+        Toast.makeText(this, "not implemented", Toast.LENGTH_SHORT).show()
     }
 
 }
